68f86f8553bfb2b992259b61d7231537c096e480
Xander Riga <xanderriga@live.com>
2020-12-11 22:24:41 -0800

    Set up boilerplate for part 12
    I don't have time to do this tonight, but this parses the input as I
    would like so I can tackle it tomorrow.

---
+++ day12/main.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require './day12/navigation'
+
+navigation = Day12::Navigation.new
+
+puts "Part 1: #{navigation.part_1}"
+
+# puts "Part 2: #{navigation.part_2}"
---
+++ day12/navigation.rb
@@ -0,0 +1,22 @@
+module Day12
+  class Navigation
+    def part_1
+      instructions
+    end
+
+    private
+
+    def instructions
+      File.read('./day12/input.txt').split("\n").map do |n|
+        Instruction.new(n[0], n[1..-1].to_i)
+      end
+    end
+  end
+
+  class Instruction
+    def initialize(action, value)
+      @action = action
+      @value = value
+    end
+  end
+end
